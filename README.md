3f4773170146922044b54eb05ce14d021ea8a44f
Karl Pettersson <karl_pettersson_1998@yahoo.com>
2016-12-24 12:13:09 +0100

    Initial commit

--- README.md
+++ README.md
@@ -5,3 +5,45 @@
 [![Coverage Status](https://coveralls.io/repos/klpn/JSONStat.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/klpn/JSONStat.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/klpn/JSONStat.jl/coverage.svg?branch=master)](http://codecov.io/github/klpn/JSONStat.jl?branch=master)
+
+This package can be used to read and write datasets in the [JSON-stat
+format](https://json-stat.org/). It is intended be used with JuliaIO
+[JSON](https://github.com/JuliaIO/JSON.jl),
+[DataFrames](https://github.com/JuliaStats/DataFrames.jl/) and
+[DataStructures](https://github.com/JuliaLang/DataStructures.jl).
+
+To read a JSON-stat file containing a dataset.
+
+```julia
+using JSONStat
+import JSON
+oecdjsonstat = JSON.parsefile("test/oecd.json",
+dicttype=DataStructures.OrderedDict)
+oecd = readjsondataset(oecdjsonstat)
+```
+
+Parses the contents of `test/oecd.json` into an `OrderedDict`, `oecd`. The
+DataFrame `oecd["datasetframe"]` will contain columns with all data dimensions
+defined in `dimension` in `oecd.json` and the values defined in `value`.
+
+Older `bundle` responses containing a map of datasets can also be read:
+
+```julia
+oecdcajsonstat = JSON.parsefile("test/oecd-canada.json",
+dicttype=DataStructures.OrderedDict)
+oecdca = readjsonbundle(oecdcajsonstat)
+```
+
+Dictionaries with labels for a specific category can be converted to
+DataFrames, e.g. to be joined with the main `datasetframe` for a dataset:
+
+```julia
+areaframe = labelframe("area", oecd)
+join(oecd["datasetframe"], areaframe, on=:area)
+```
+
+Ordered dicts with datasets can be written to JSON-stat:
+
+```julia
+JSON.json(writejsondataset(oecd), 1)
+```
--- src/JSONStat.jl
+++ src/JSONStat.jl
@@ -1,5 +1,121 @@
 module JSONStat
 
-# package code goes here
+using DataStructures, DataFrames
+
+export readjsondataset, readjsonbundle, writejsondataset, labelframe
+
+
+optkeys = Dict(
+		"dataset" => [
+			"version"; "class"; "role"; "label"; "updated"; "source";
+			"extension"; "href";"link"; "note"; "error"
+			],
+		"dimid" => [
+			"label"
+			],
+		"category" => [
+			"label"; "child"; "coordinates"; "unit"; "note"
+			]
+	)
+
+function dimframe(id, dimjsonstat)
+	dimid = dimjsonstat[id]
+	dimcat = dimid["category"]
+	dimdict = DataStructures.OrderedDict()
+	dimdict["category"] = DataStructures.OrderedDict()
+	if haskey(dimcat, "index")
+		dimdata = dimcat["index"]
+	else
+		dimdata = dimcat["label"]
+	end
+	for key in keys(dimid)
+		if key in optkeys["dimid"]
+			dimdict[key] = dimid[key]
+		end
+	end
+	for key in keys(dimcat)
+		if key in optkeys["category"]
+			dimdict["category"][key] = dimcat[key]
+		end
+	end
+	dimframe = DataFrame()
+	if typeof(dimdata)<:DataStructures.OrderedDict
+		dimframe[Symbol(id)] = collect(keys(dimdata))
+	else
+		dimframe[Symbol(id)] = dimdata
+	end
+	dimdict["dimframe"] = dimframe
+	id => dimdict
+end
+
+function readjsondataset(datasetjsonstat)
+	if haskey(datasetjsonstat, "version")
+		idarr = datasetjsonstat["id"]
+	else
+		idarr = datasetjsonstat["dimension"]["id"]
+	end
+	dimension = DataStructures.OrderedDict(map((x)->
+		dimframe(x, datasetjsonstat["dimension"]), idarr))
+	dimkeys = collect(keys(dimension))
+	datasetframe = dimension[dimkeys[1]]["dimframe"]
+	for i in 2:size(dimkeys)[1]
+		datasetframe = join(datasetframe, dimension[dimkeys[i]]["dimframe"],
+			kind = :cross)
+	end
+	datasetframe[:value] = datasetjsonstat["value"]
+	datasetdict = DataStructures.OrderedDict()
+	for key in keys(datasetjsonstat)
+		if key in optkeys["dataset"]
+			datasetdict[key] = datasetjsonstat[key]
+		end
+	end
+	datasetdict["dimension"] = dimension
+	datasetdict["datasetframe"] = datasetframe
+	datasetdict
+end
+
+function labelframe(id, datasetdict)
+	labeldict = datasetdict["dimension"][id]["category"]["label"]
+	labelframe = DataFrame()
+	labelframe[Symbol(id)] = collect(keys(labeldict))
+	labelframe[Symbol("$(id)_label")] = collect(values(labeldict))
+	labelframe
+end
+
+function readjsonbundle(bundlejsonstat)
+	bundledict = DataStructures.OrderedDict()
+	for key in keys(bundlejsonstat)
+		if haskey(bundlejsonstat[key], "value")
+			bundledict[key] = readjsondataset(bundlejsonstat[key])
+		end
+	end
+	bundledict
+end
+
+function writedimid(id, datasetjsonstat)
+	datasetjsonstat["id"] = vcat(datasetjsonstat["id"], id)
+	dimid = datasetjsonstat["dimension"][id]
+	dimcat = dimid["category"]
+	dimframe = dimid["dimframe"]
+	dimsize = size(dimframe)[1]
+	datasetjsonstat["size"] = vcat(datasetjsonstat["size"], dimsize)
+	dimzip = zip(dimframe[Symbol(id)], collect(0:dimsize-1))
+	dimcat["index"] = DataStructures.OrderedDict(dimzip)
+	delete!(dimid, "dimframe")
+end
+
+function writejsondataset(datasetdict)
+	datasetjsonstat = deepcopy(datasetdict)
+	if !(haskey(datasetjsonstat, "version"))
+		datasetjsonstat["version"] = "2.0"
+	end
+	datasetjsonstat["id"] = []
+	datasetjsonstat["size"] = []
+	datasetjsonstat["value"] = datasetjsonstat["datasetframe"][:value]
+	delete!(datasetjsonstat, "datasetframe")
+	idarr = collect(keys(datasetjsonstat["dimension"]))
+	foreach((x)->writedimid(x, datasetjsonstat), idarr)
+	datasetjsonstat
+end
 
 end # module
